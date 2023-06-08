41275ef57e8eb9a956ab58091ac22d382bfef110
William May <wjrm500@gmail.com>
2023-06-08 21:30:22 +0800

    Gradio interface

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
 import os
 
 from dotenv import load_dotenv
+import gradio
 import openai
 
 load_dotenv()
@@ -17,8 +17,13 @@ message_history = [
 
 total_tokens_used = 0
 
-while True:
-    user_input = input("User: ")
+def accountant_message(total_tokens_used):
+    return f"You've spent ${COST_PER_1K_TOKENS * total_tokens_used / 1000:.3f} USD on this conversation."
+
+def chat(user_input):
+    global message_history
+    global total_tokens_used
+
     message_history.append({"role": "user", "content": user_input})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -26,8 +31,21 @@ while True:
     )
     assistant_output = completion.choices[0].message.content
     message_history.append({"role": "assistant", "content": assistant_output})
-    print("Assistant:", assistant_output)
 
     tokens_used = completion.usage.total_tokens
     total_tokens_used += tokens_used
-    print(f"Irritating accountant: You've spent ${COST_PER_1K_TOKENS * total_tokens_used / 1000:.3f} USD on this conversation.")
+
+    return assistant_output, accountant_message(total_tokens_used)
+
+demo = gradio.Interface(
+    fn=chat,
+    inputs=gradio.inputs.Textbox(label="User input", lines=2, placeholder="Say something..."),
+    outputs=[
+        gradio.outputs.Textbox(label="Assistant"),
+        gradio.outputs.Textbox(label="Accountant")
+    ],
+    title="Spanish Language Tutor",
+    description="A Spanish language tutor powered by GPT3.5.",
+)
+
+demo.launch()
