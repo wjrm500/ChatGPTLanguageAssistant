9bf0f0dc68a11dc4c28191175430ec266aafe319
William May <wjrm500@gmail.com>
2023-06-08 11:05:33 +0800

    Keeping track of expenditure

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+COST_PER_1K_TOKENS = 0.002 # USD
 SYSTEM_PROMPT = open("system_prompt.txt", "r").read()
 
 message_history = [
     {"role": "system", "content": SYSTEM_PROMPT}
 ]
+
+total_tokens_used = 0
 
 while True:
     user_input = input("User: ")
@@ -24,3 +27,7 @@ while True:
     assistant_output = completion.choices[0].message.content
     message_history.append({"role": "assistant", "content": assistant_output})
     print("Assistant:", assistant_output)
+
+    tokens_used = completion.usage.total_tokens
+    total_tokens_used += tokens_used
+    print(f"Irritating accountant: You've spent ${COST_PER_1K_TOKENS * total_tokens_used / 1000:.3f} USD on this conversation.")
