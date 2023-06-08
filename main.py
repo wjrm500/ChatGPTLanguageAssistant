edf75e419c54c540bbd0769d0b9d8fcef77c8bff
William May <wjrm500@gmail.com>
2023-06-08 10:52:25 +0800

    Basic program with system prompt

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import json
+import os
+
+from dotenv import load_dotenv
 import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+SYSTEM_PROMPT = open("system_prompt.txt", "r").read()
+
+message_history = [
+    {"role": "system", "content": SYSTEM_PROMPT}
+]
+
+while True:
+    user_input = input("User: ")
+    message_history.append({"role": "user", "content": user_input})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history
+    )
+    assistant_output = completion.choices[0].message.content
+    message_history.append({"role": "assistant", "content": assistant_output})
+    print("Assistant:", assistant_output)
