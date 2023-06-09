8f686a25f004f47cb1770b94b020a4ea0c1a8d70
William May <wjrm500@gmail.com>
2023-06-09 11:29:48 +0800

    Major change to process, to translate sentence by sentence for more interesting corrections

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor
 import os
+import re
 
 from dotenv import load_dotenv
 import gradio
@@ -9,10 +11,12 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 COST_PER_1K_TOKENS = 0.002 # USD
-SYSTEM_PROMPT = open("system_prompt.txt", "r").read()
+PROMPT_SYSTEM_MAIN = open("prompts/system_main.txt", "r").read()
+PROMPT_ANALYSE_CORRECTION = open("prompts/analyse_correction.txt", "r").read()
+PROMPT_TRANSLATE_SENTENCE = open("prompts/translate_sentence.txt", "r").read()
 
-message_history = [
-    {"role": "system", "content": SYSTEM_PROMPT}
+main_message_history = [
+    {"role": "system", "content": PROMPT_SYSTEM_MAIN}
 ]
 
 total_tokens_used = 0
@@ -21,21 +25,77 @@ def accountant_message(total_tokens_used):
     return f"You've spent ${COST_PER_1K_TOKENS * total_tokens_used / 1000:.3f} USD on this conversation."
 
 def chat(user_input):
-    global message_history
+    global main_message_history
     global total_tokens_used
 
-    message_history.append({"role": "user", "content": user_input})
+    split_regex = r"(?<=[.!?])\s+"
+    input_sentences = re.split(split_regex, user_input)
+    corrected_sentences = []
+    for input_sentence in input_sentences:
+        prompt = PROMPT_TRANSLATE_SENTENCE.format(sentence=input_sentence)
+        print("Making request...")
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        ai_output = completion.choices[0].message.content.replace("\"", "")
+        corrected_sentences.append(ai_output)
+        tokens_used = completion.usage.total_tokens
+        total_tokens_used += tokens_used
+    
+    correction_explanations = []
+    for input_sentence, corrected_sentence in zip(input_sentences, corrected_sentences):
+        prompt = PROMPT_ANALYSE_CORRECTION.format(input_sentence=input_sentence, corrected_sentence=corrected_sentence)
+        print("Making request...")
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        ai_output = completion.choices[0].message.content
+        correction_explanations.append(ai_output)
+        tokens_used = completion.usage.total_tokens
+        total_tokens_used += tokens_used
+    correction_explanations = [y.split("|")[1].strip() for x in correction_explanations for y in x.split("\n") if "|" in y]
+    correction_explanation = "\n".join([f"{i}. {x}" for i, x in enumerate(correction_explanations, 1)])
+
+    main_message_history.append({"role": "user", "content": user_input})
+    print("Making request...")
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=message_history
+        messages=main_message_history
     )
-    assistant_output = completion.choices[0].message.content
-    message_history.append({"role": "assistant", "content": assistant_output})
-
+    ai_output = completion.choices[0].message.content
+    main_message_history.append({"role": "assistant", "content": ai_output})
     tokens_used = completion.usage.total_tokens
     total_tokens_used += tokens_used
+    
+    final_ai_output = """
+Input
+-----
+{user_input}
 
-    return assistant_output, accountant_message(total_tokens_used)
+Correction
+----------
+{correction}
+
+Explanation of corrections
+--------------------------
+{explanation}
+
+Response
+--------
+{response}
+""".format(
+    user_input=user_input,
+    correction=" ".join(corrected_sentences),
+    explanation=correction_explanation,
+    response=ai_output
+)
+    return final_ai_output, accountant_message(total_tokens_used)
 
 demo = gradio.Interface(
     fn=chat,
