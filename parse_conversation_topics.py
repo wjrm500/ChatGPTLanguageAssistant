b2e886c13ce3a14a549a3abfe9cbf296e2aa4cbd
William May <wjrm500@gmail.com>
2023-06-16 19:01:12 +0800

    Script to parse topics copied and pasted from Wikipedia

---
+++ parse_conversation_topics.py
@@ -0,0 +1,26 @@
+import re
+
+def split_by_delimiters(text, delimiters):
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    pieces = re.split(regexPattern, text)
+    return pieces
+
+def split_by_uppercase(text):
+    return re.split('(?<=[a-z])(?=[A-Z])', text)
+
+with open("conversation_topics.txt", "r", encoding="utf-8") as fh:
+    content = fh.read()
+    delimiters = ["\n", "•", "→", " - ", "|"]
+    pieces = split_by_delimiters(content, delimiters)
+    pieces = [stripped for piece in pieces if (stripped := piece.strip())]
+    allah_piece = next(piece for piece in pieces if piece.startswith("AllahBelief"))
+    acosmism_piece = next(piece for piece in pieces if piece.startswith("AcosmismAgnosticism"))
+    pieces.remove(allah_piece)
+    pieces.remove(acosmism_piece)
+    allah_pieces = split_by_uppercase(allah_piece)
+    acosmism_pieces = split_by_uppercase(acosmism_piece)
+    pieces.extend(allah_pieces)
+    pieces.extend(acosmism_pieces)
+    
+    with open("conversation_topics_parsed.txt", "w", encoding="utf-8") as fh:
+        fh.write("\n".join(pieces))
