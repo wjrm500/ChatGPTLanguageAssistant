072ffee31fb390a0cf3e8dfa2122f10a5567d3fe
William May <wjrm500@gmail.com>
2023-06-17 11:48:00 +0800

    Adding mypy check to check.bat

--- check.bat
+++ check.bat
@@ -2,7 +2,13 @@
 
 echo Running black...
 call black .
+
 echo.
 
 echo Running flake8...
 call flake8 . --exclude venv/ --ignore E501
+
+echo.
+
+echo Running mypy...
+call mypy . --ignore-missing-imports
