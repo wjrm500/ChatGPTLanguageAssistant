cc368603cf0b31d366fe6f34d09292c59b2e664f
William May <wjrm500@gmail.com>
2023-06-17 11:38:46 +0800

    Fix

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ output_tokens_used = 0
 
 
 def conversation_topic():
-    return "Space exploration"
     with open("conversation_topics_parsed.txt", "r", encoding="utf-8") as file:
         lines = file.readlines()
     return random.choice(
